fd8699dd46bc8a240d7a1b40a2755940c06876f7
pame schurrer <schurrerpame@gmail.com>
2025-04-19 22:06:27 +0200

    modif.

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,10 +8,6 @@ const blog = defineCollection({
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
 	}),
 });
 
