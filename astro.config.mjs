53fac32cafaca13941164fe9107f471df632af67
pame schurrer <schurrerpame@gmail.com>
2025-04-21 11:19:57 +0200

    modif.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://pameschurrer.com/astro-web/',
+	base: '/astro-web/',
 	integrations: [mdx(), sitemap()],
 });
